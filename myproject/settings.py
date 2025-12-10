ae9de8842877ccf480ff90831c5172e944c5a3b7
Lhoel Donayre <jokerbelike12@gmail.com>
2025-12-10 20:29:49 +0800

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v)rnb+p9y%429m=o9s@iwle&ps_ad6)+0gz)i0h(9gitb*woi7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-portfolio-4-bk2w.onrender.com']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "django-portfolio-4-bk2w.onrender.com"]
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
